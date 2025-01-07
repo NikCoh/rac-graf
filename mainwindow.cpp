34084f657fb69c7fc23c2bb1ce105609d6f69cd8
mucklara <146102334+mucklara@users.noreply.github.com>
2025-01-07 01:29:54 +0100

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <QFileDialog>
 #include <iostream>
 #include <QProces>
+#include <QTimer>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
@@ -36,5 +37,24 @@ void MainWindow::on_pushButton_2_clicked()
     }
 
     std::cout << "Output:" << process.readAllStandardOutput().toStdString();
+
+    glm::mat4 matrix = glm::mat4(1.0f);
+    matrix[0][0] = 1.0f;  matrix[0][1] = 0.0f; matrix[0][2] = 0.0f; matrix[0][3] = 0.0f;
+    matrix[1][0] = 0.0f;  matrix[1][1] = 1.0f; matrix[1][2] = 0.0f; matrix[1][3] = 0.0f;
+    matrix[2][0] = 0.0f;  matrix[2][1] = 0.0f; matrix[2][2] = 1.0f; matrix[2][3] = 0.0f;
+    matrix[3][0] = -0.2f; matrix[3][1] = 0.0f; matrix[3][2] = 4.2f; matrix[3][3] = 1.0f;
+
+    ui->widget->addMesh("../../objects/oblak-AC_off.obj");
+
+    ui->widget->meshes[ui->widget->meshes.size()-1].modelMatrix = matrix;
+
+    ui->widget->update();
+
+    QTimer::singleShot(10000, this, [this]() {
+
+        ui->widget->meshes.pop_back();
+        ui->widget->update();
+
+    });
 }
 
